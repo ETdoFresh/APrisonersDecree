f63abdc729f7dc2540096164cce630867e170006
ETdoFresh <ETdoFresh@gmail.com>
2018-11-30 21:36:41 -0600

    Implement timer to destroy gameobjects.

---
+++ Assets/DestroyTimer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DestroyTimer : MonoBehaviour
+{
+    public float timer;
+    public Text timerText;
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+            OnFinishTimer();
+
+        timerText.text = timer.ToString("0.000");
+    }
+
+    public void StartTimer()
+    {
+        enabled = true;
+    }
+
+    private void OnFinishTimer()
+    {
+        timer = 0;
+        foreach (var playerUnit in FindObjectsOfType<PlayerUnit>())
+            Destroy(playerUnit.gameObject);
+        enabled = false;
+    }
+}
