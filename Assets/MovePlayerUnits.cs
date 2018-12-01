308dfb74203b2fdf013229196b1ad0c8fb4a70e1
ETdoFresh <ETdoFresh@gmail.com>
2018-11-30 21:28:21 -0600

    Implement Go button so that path 1 gets X player units to travel it.

---
+++ Assets/MovePlayerUnits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class MovePlayerUnits : MonoBehaviour
+{
+    public Transform destination;
+    public Text numberOfUnitsText;
+
+    public void MovePlayerUnit()
+    {
+        var units = FindObjectsOfType<PlayerUnit>();
+        int numberOfUnits = int.Parse(numberOfUnitsText.text);
+        numberOfUnits = Math.Min(numberOfUnits, units.Length);
+
+        for (int i = 0; i < numberOfUnits; i++)
+        {
+            var navmeshagent = units[i].GetComponent<NavMeshAgent>();
+            navmeshagent.enabled = false;
+            units[i].transform.position = destination.position;
+            navmeshagent.enabled = true;
+        }
+    }
+}
