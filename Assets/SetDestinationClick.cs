e27fcee614c5b38826c38d1b67d586203a054af9
ETdoFresh <ETdoFresh@gmail.com>
2018-11-30 20:58:09 -0600

    Create a quick prototype using navmesh and spheres.

---
+++ Assets/SetDestinationClick.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class SetDestinationClick : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            MoveToMouse();
+    }
+
+    private void MoveToMouse()
+    {
+        RaycastHit hit;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+            navMeshAgent.SetDestination(hit.point);
+    }
+}
