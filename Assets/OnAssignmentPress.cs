6f582104f22239a7bbe0e9ee88c05827814c15a5
ETdoFresh <ETdoFresh@gmail.com>
2018-11-30 21:08:58 -0600

    Implement + and - buttons for count (path selection assignment).

---
+++ Assets/OnAssignmentPress.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnAssignmentPress : MonoBehaviour {
+    public void OnMinusPress()
+    {
+        var textComponent = GetComponent<Text>();
+        int count = int.Parse(textComponent.text);
+        count--;
+        textComponent.text = count.ToString();
+    }
+
+    public void OnPlusPress()
+    {
+        var textComponent = GetComponent<Text>();
+        int count = int.Parse(textComponent.text);
+        count++;
+        textComponent.text = count.ToString();
+    }
+}
